9d8ae2996fbeb4689ec8742bbf942c5ecb331c16
Alex Chera <123866063+gabirelul@users.noreply.github.com>
2025-05-25 21:35:22 +0300

    first

--- main/src/main/java/model/Vehicle.java
+++ main/src/main/java/model/Vehicle.java
@@ -10,13 +10,11 @@ public interface Vehicle {
     int getId();
     void setId(int id);
 
-    /** Returns the type of vehicle ("CAR" or "MOTORCYCLE") */
     String getType();
-    /** Sets the type of vehicle */
+
     void setType(String type);
 
-    /** Returns the owner of the vehicle */
     Owner getOwner();
-    /** Sets the owner of the vehicle */
+
     void setOwner(Owner owner);
 }
